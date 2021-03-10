c6e18d4b2341deee6caa789517b2ded9b146bb96
Alexander <alexssjr2002@gmail.com>
2021-03-10 13:17:44 -0500

    readData() method added

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,8 +1,30 @@
 package ui;
 
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
+	private static final String SEPARATOR = " ";
+	
+	public void readData () throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String line = br.readLine();
+		line = br.readLine();
+		String[] parts = line.split(SEPARATOR);
+		int [] prices = new int [parts.length];
+		for (int j = 0; j < parts.length; j++) {
+			prices[j] = Integer.parseInt(parts[j]);
+		}
+		line = br.readLine();
+		int money = Integer.parseInt(line);
+		
+		br.close();	
+
+	}
 
 	public static void main(String[] args) {
 		
